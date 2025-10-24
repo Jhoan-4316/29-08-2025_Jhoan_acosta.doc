c711dfa85ad4d91cade0e6f318768d113091cf26
Jhoan-4316 <jracostap@udistrital.edu.co>
2025-10-24 13:17:46 -0500

    Add files via upload

---
+++ Programa_7_2.c
@@ -0,0 +1,19 @@
+// JHOAN RAUL ACOSTA PIEDRAHITA 
+// 24 DE OCTUBRE DE 2025
+//
+
+#include <stdio.h>   // Librería 
+
+int main()  // Función principal
+{
+    if (0) // 0 significa falso, este bloque no se ejecuta
+    {
+        printf("Esta instrucción nunca se ejecuta\n");
+        printf("porque la condición siempre es falsa (0).\n");
+    }
+
+    if (-8) // Todo número distinto de 0 es verdadero, se ejecuta
+        printf("Esta instrucción siempre se ejecuta.\n");
+
+    return 0; // Fin del programa
+}
